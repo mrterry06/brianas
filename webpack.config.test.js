5d397d798c57ed9b023daa6fd2c9871d0ee20485
Richard Park <objectiv@gmail.com>
2015-11-22 21:23:56 -0500

    chore: clean up webpack test config

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -5,20 +5,13 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'inline-source-map',
 
-  output: {
-    filename: '[name].js',
-    path: path.resolve('./target'),
-    publicPath: '/'
-  },
-
   resolve: {
     root: path.resolve('src')
   },
 
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
-      {test: /\.html$/, loader: 'raw'}
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel'}
     ]
   }
 };
