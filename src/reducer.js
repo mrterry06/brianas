308eb04b9bd0eefd5638713f3acdbff0885bd123
Richard Park <objectiv@gmail.com>
2016-02-27 23:23:08 -0500

    chore(package): update dependencies
    - update react-router-redux to v4
    - resolve breaking changes introduced by react-router-redux v4

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-import { routeReducer } from 'react-router-redux';
+import { routerReducer } from 'react-router-redux';
 import { counterReducer } from 'modules/counter/reducer';
 
 
 export default combineReducers({
   counter: counterReducer,
-  routing: routeReducer
+  routing: routerReducer
 });
