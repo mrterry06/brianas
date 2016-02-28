9034c93f222bea461b5235dae08224cbfb4c682b
Richard Park <objectiv@gmail.com>
2016-02-28 00:41:53 -0500

    chore(build): set process.env.NODE_ENV
    - set process.env.NODE_ENV for dev and test builds

--- webpack.test.js
+++ webpack.test.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 
+// plugins
+const DefinePlugin = webpack.DefinePlugin;
+
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -13,5 +16,11 @@ module.exports = {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel'}
     ]
-  }
+  },
+
+  plugins: [
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('test')
+    })
+  ]
 };
