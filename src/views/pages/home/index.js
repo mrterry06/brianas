7871f3aa8f598dc339b1ff2fea8da93f39698903
Scott Terry <scottterry@scottterry-mac.attlocal.net>
2019-03-20 18:49:15 -0500

    adding username fields

--- src/views/pages/home/index.js
+++ src/views/pages/home/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 
 function HomePage() {
-  return <h1>Home</h1>;
+  return (
+  <div>
+    <h1>Home</h1>
+    <div>
+      <label>Username:</label>
+      <input type='text'/>
+      <label>Password:</label>
+      <input type='password'/> 
+     </div>
+  </div>
+  )
 }
 
 export default HomePage;
